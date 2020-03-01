2e3a43ed0703dfc718994989dc4defd7ca3e8710
Eric <zero5652pzj@163.com>
2020-03-01 13:41:47 +0800

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+file = "e:/sanwen.txt"
+
+def document(filepath):
+	with open(filepath, 'r') as f:
+		for _ in range(30):
+			text = f.readline()
+			yield text
+
+doc = document(file)
+words = 'Treat the image'
+for _ in range(30):
+	texts = next(doc)
+	if words in texts:
+		print(texts)
